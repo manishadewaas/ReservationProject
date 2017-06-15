471ed127b8708adecd279a8455a44630d521b45a
manishadewaas <manishadewaas@gmail.com>
2017-06-15 22:34:34 +0530

    add metroTile1,metroTile2,metroTile3,metroTile4,metroTile5.

--- ReservationSystem/ReservationSystem/Admin.Designer.cs
+++ ReservationSystem/ReservationSystem/Admin.Designer.cs
@@ -28,34 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroTile1 = new MetroFramework.Controls.MetroTile();
+            this.metroTile2 = new MetroFramework.Controls.MetroTile();
+            this.metroTile3 = new MetroFramework.Controls.MetroTile();
+            this.metroTile4 = new MetroFramework.Controls.MetroTile();
+            this.metroTile5 = new MetroFramework.Controls.MetroTile();
             this.SuspendLayout();
             // 
-            // metroLabel1
+            // metroTile1
             // 
-            this.metroLabel1.AutoSize = true;
-            this.metroLabel1.Location = new System.Drawing.Point(23, 100);
-            this.metroLabel1.Name = "metroLabel1";
-            this.metroLabel1.Size = new System.Drawing.Size(71, 19);
-            this.metroLabel1.TabIndex = 0;
-            this.metroLabel1.Text = "UserName";
+            this.metroTile1.ActiveControl = null;
+            this.metroTile1.Location = new System.Drawing.Point(23, 83);
+            this.metroTile1.Name = "metroTile1";
+            this.metroTile1.Size = new System.Drawing.Size(146, 82);
+            this.metroTile1.TabIndex = 1;
+            this.metroTile1.Text = "Handle Admin Details";
+            this.metroTile1.UseSelectable = true;
+            // 
+            // metroTile2
+            // 
+            this.metroTile2.ActiveControl = null;
+            this.metroTile2.Location = new System.Drawing.Point(191, 83);
+            this.metroTile2.Name = "metroTile2";
+            this.metroTile2.Size = new System.Drawing.Size(141, 82);
+            this.metroTile2.TabIndex = 2;
+            this.metroTile2.Text = "Handle Driver Details";
+            this.metroTile2.UseSelectable = true;
+            // 
+            // metroTile3
+            // 
+            this.metroTile3.ActiveControl = null;
+            this.metroTile3.Location = new System.Drawing.Point(348, 83);
+            this.metroTile3.Name = "metroTile3";
+            this.metroTile3.Size = new System.Drawing.Size(154, 82);
+            this.metroTile3.TabIndex = 3;
+            this.metroTile3.Text = "Handle Bus Details";
+            this.metroTile3.UseSelectable = true;
+            // 
+            // metroTile4
+            // 
+            this.metroTile4.ActiveControl = null;
+            this.metroTile4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.metroTile4.Location = new System.Drawing.Point(92, 202);
+            this.metroTile4.Name = "metroTile4";
+            this.metroTile4.Size = new System.Drawing.Size(143, 80);
+            this.metroTile4.TabIndex = 4;
+            this.metroTile4.Text = "Reservation";
+            this.metroTile4.UseSelectable = true;
+            // 
+            // metroTile5
+            // 
+            this.metroTile5.ActiveControl = null;
+            this.metroTile5.Location = new System.Drawing.Point(264, 202);
+            this.metroTile5.Name = "metroTile5";
+            this.metroTile5.Size = new System.Drawing.Size(143, 80);
+            this.metroTile5.TabIndex = 5;
+            this.metroTile5.Text = "view report";
+            this.metroTile5.UseSelectable = true;
             // 
             // Admin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(300, 300);
-            this.Controls.Add(this.metroLabel1);
+            this.ClientSize = new System.Drawing.Size(555, 323);
+            this.Controls.Add(this.metroTile5);
+            this.Controls.Add(this.metroTile4);
+            this.Controls.Add(this.metroTile3);
+            this.Controls.Add(this.metroTile2);
+            this.Controls.Add(this.metroTile1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Name = "Admin";
-            this.Text = "Admin";
+            this.Text = "Bus Seat Reservation System";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroTile metroTile1;
+        private MetroFramework.Controls.MetroTile metroTile2;
+        private MetroFramework.Controls.MetroTile metroTile3;
+        private MetroFramework.Controls.MetroTile metroTile4;
+        private MetroFramework.Controls.MetroTile metroTile5;
     }
 }
 
